87ad86ba956f92bdadb98952ecd9ee9023c923d4
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2018-12-04 16:54:14 -0800

    Include Bootstrap CSS in the app

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
