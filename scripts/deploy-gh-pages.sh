ca4f8d989246a19a00ca76a2609af82cbd6b718e
Ryan Loomba <ryan@loomba.io>
2019-02-18 22:13:07 -0800

    update deploy stuff

---
+++ scripts/deploy-gh-pages.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git commit -am "Save uncommited changes (WIP)"
+git branch --delete --force gh-pages
+git checkout --orphan gh-pages
+git add -f dist
+git commit -m "Rebuild GitHub pages"
+git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout -
