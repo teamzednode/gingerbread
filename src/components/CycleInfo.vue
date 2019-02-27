a33ba6945c23359ad8d93cb6a0538b115fe8e2be
Rohit Verma <rohitverma@live.ca>
2019-02-27 14:06:01 -0400

    adding rewards share and amount

--- src/components/CycleInfo.vue
+++ src/components/CycleInfo.vue
@@ -28,7 +28,9 @@ export default {
       currentCycle: this.$route.params.cycle,
       contractsDataFields: [
         {key: 'contractId', label: 'Delegator'},
-        {key: 'contractData', label: 'Delegator Balance'}
+        {key: 'contractData', label: 'Delegator Balance'},
+        {key: 'rewardsShare', label: 'Rewards Share'},
+        {key: 'rewardsAmount', label: 'Rewards Amount'}
       ],
       contractsData: [],
       tezosHelper: new TezosHelper(),
@@ -42,10 +44,18 @@ export default {
     'cycle'
   ]),
   methods: {
+    setContractShare: function (stakingBalance, totalRewards) {
+      for (let i = 0; i < this.contractsData.length; i++) {
+        console.log(this.contractsData[i])
+        const rewardsSharePercentage = (this.contractsData[i].contractData.balance / stakingBalance)
+        this.contractsData[i]['rewardsShare'] = (rewardsSharePercentage * 100).toFixed(2) + '%'
+        this.contractsData[i]['rewardsAmount'] = this.tezosHelper.formatTezosNumericalData(
+          totalRewards * (1 - (parseFloat(this.user.fee_percent) / 100)) * (rewardsSharePercentage))
+      }
+    },
     compareBalance: function (a, b) {
       const balanceA = parseFloat(a.contractData.balance)
       const balanceB = parseFloat(b.contractData.balance)
-
       let comparisonValue = 0
 
       if (balanceA < balanceB) {
@@ -70,11 +80,16 @@ export default {
     this.cycleMetaData.snapshotNumber = this.snapshot.snapshotData[tezosRpc.cycle]
     this.cycleMetaData.snapshotBlockNumber = this.snapshot.snapshotblockNumberData[tezosRpc.cycle]
     this.cycleMetaData.stakingBalance = this.tezosHelper.formatTezosNumericalData(cycleData.staking_balance)
-    this.cycleMetaData.endorsingRewards = this.tezosHelper.formatTezosNumericalData(this.cycle.data[this.currentCycle][this.user.baker_tz_address]['endorsingRewards'])
-    this.cycleMetaData.bakingRewards = this.tezosHelper.formatTezosNumericalData(this.cycle.data[this.currentCycle][this.user.baker_tz_address]['bakingRewards'])
+    const endorsingRewards = this.cycle.data[this.currentCycle][this.user.baker_tz_address]['endorsingRewards']
+    const bakingRewards = this.cycle.data[this.currentCycle][this.user.baker_tz_address]['bakingRewards']
+    const totalRewards = endorsingRewards + bakingRewards
+    this.cycleMetaData.endorsingRewards = this.tezosHelper.formatTezosNumericalData(endorsingRewards)
+    this.cycleMetaData.bakingRewards = this.tezosHelper.formatTezosNumericalData(bakingRewards)
+    this.cycleMetaData.totalRewards = this.tezosHelper.formatTezosNumericalData(totalRewards)
 
     const contractIdsArray = await tezosRpc.getSnapshotDelegateContractIds()
     this.contractsData = await tezosRpc.getContractsData(contractIdsArray)
+    this.setContractShare(cycleData.staking_balance, totalRewards)
     this.contractsData.sort(this.compareBalance)
     this.loadingText = ''
   }
